f375bddc8034e14be91042b867d3ec3c8d276d5f
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-03-01 22:07:44 +0200

    add fetch

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,1 +1,9 @@
-function fetchCountries(name) {}
+export async function fetchCountries(name) {
+  const response = await fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  );
+  if (!response.ok) {
+    throw new Error(response.status);
+  }
+  return await response.json();
+}
