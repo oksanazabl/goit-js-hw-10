0165ec984155c8f171594f6e5d836cdf280d7062
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-03-01 19:16:21 +0200

    update json

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
 
-Notify.info('Too many matches found. Please enter a more specific name.');
+// Notify.info('Too many matches found. Please enter a more specific name.');
 
-Notify.failure('Oops, there is no country with that name');
+// Notify.failure('Oops, there is no country with that name');
