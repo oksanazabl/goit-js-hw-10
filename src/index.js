20c8eed683f47ff88bcdc2d7256453180913df9c
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-03-01 23:54:30 +0200

    add alert

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import { fetchCountries } from './js/fetchCountries';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { addCountryList, addCountryInfo } from './js/markup';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
-// Notify.info('Too many matches found. Please enter a more specific name.');
+refs = {
+  input: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
 
-// Notify.failure('Oops, there is no country with that name');
+function alertNoSuchCountry() {
+  Notiflix.Notify.failure('Oops, there is no country with that name');
+}
+
+function alertSpecificName() {
+  Notiflix.Notify.info(
+    'Too many matches found. Please enter a more specific name.'
+  );
+}
