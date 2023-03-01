fcea2c1d33255c61593c802eab34382cbacde365
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-03-01 09:11:29 +0200

    add Notify

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './js/fetchCountries';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
+
+Notify.info('Too many matches found. Please enter a more specific name.');
+
+Notify.failure('Oops, there is no country with that name');
