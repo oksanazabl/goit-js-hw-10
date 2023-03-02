88dff0c239723fe31d72166a45e48aafcb703357
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-03-02 11:58:50 +0200

    fix refs

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
-refs = {
+const refs = {
   input: document.querySelector('#search-box'),
   countryList: document.querySelector('.country-list'),
   countryInfo: document.querySelector('.country-info'),
